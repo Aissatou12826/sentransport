3a89b54d668740d8348202d75638dec551cc2269
Aissatou Ibrahima Ka <kastou749@gmail.com>
2026-04-20 23:25:48 +0000

    Exercices 1, 2 et 3 termines

---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+  return (
+    <div className="statistique">
+      <p className="stat-chiffre">10</p>
+      <p className="stat-libelle">lignes</p>
+    </div>
+  );
+}
+
+export default Statistique;
